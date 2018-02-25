f856291dcd32220cc70a55edf04abdbca534bb90
Kevin Xue <ivexnuke@gmail.com>
2018-02-24 22:44:23 -0500

    Implemented flood fill, rowcol to index conversion moved to constants, background color darkened

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,17 @@
 const CONSTANTS ={
     width: 960,
     height: 680,
-    cellSize: 100,
+    cellSize: 75,
     gridWidth: 5,
-    gridHeight: 5
+    gridHeight: 5,
+
+    getIndex: function(row, col){
+        return row * this.gridWidth + col
+    },
+    getRowCol: function(index){
+        return {
+            row: Math.floor(index / CONSTANTS.gridWidth),
+            col: index % CONSTANTS.gridWidth
+        }
+    },
 }
--- js/scenes/main.js
+++ js/scenes/main.js
@@ -1,12 +1,17 @@
-const testGrid = [
-    'R', 'O', 'I', 'I', 'L',
-    'T', 'L', 'L', 'O', 'C',
-    'L', 'I', 'T', 'I', 'C',
-    'C', 'L', 'C', 'I', 'T',
-    'L', 'T', 'O', 'C', 'F'
-];
+const testLevel = {
+    map:[
+        'C', 'O', 'I', 'L', 'T',
+        'T', 'C', 'C', 'O', 'C',
+        'L', 'I', 'T', 'I', 'C',
+        'C', 'L', 'C', 'I', 'T',
+        'L', 'T', 'O', 'C', 'C'
+    ],
+    roger: new Phaser.Point(0, 0),
+    fBI: new Phaser.Point(2, 2),
+    bitCoin: new Phaser.Point(4, 4)
+};
 
-const gridMap = {
+const gridToKey = {
     F: 'FBI',
     R: 'Roger',
     T: 'perpendicularTails',
@@ -16,31 +21,39 @@ const gridMap = {
     O: 'oneTail'
 };
 
+//clockwise from 12 o clock. Matches images
+const CONNECTIONS = {
+    T:[
+        new Phaser.Point(0, -1), 
+        new Phaser.Point(1, 0),
+        new Phaser.Point(0, 1)
+    ],
+    L:[
+        new Phaser.Point(0, -1), 
+        new Phaser.Point(1, 0)
+    ],
+    I:[
+        new Phaser.Point(0, -1),
+        new Phaser.Point(0, 1)
+    ],
+    C: [
+        new Phaser.Point(0, -1),
+        new Phaser.Point(1, 0),
+        new Phaser.Point(0, 1),
+        new Phaser.Point(-1, 0)
+    ],
+    O:[        
+        new Phaser.Point(0, -1),
+    ]
+};
+
 const mainState = {
     nodeGrid: null,
     create: function(){
         console.log('main');
-        this.populateGrid(testGrid);
-    },
+        this.populateGrid(testLevel.map);
+        this.floodFillFrom(0, 0);
 
-    fillGrid: function(key){
-        if(this.nodeGrid === null){
-            this.nodeGrid = this.add.group();
-        }
-        const startX = (game.width  - CONSTANTS.gridWidth * CONSTANTS.cellSize) * 0.5;
-        const startY = (game.height - CONSTANTS.gridHeight * CONSTANTS.cellSize) * 0.5;
-    
-        for(let row = 0; row < CONSTANTS.gridHeight; row++){
-            for(let col = 0; col < CONSTANTS.gridWidth; col++){
-
-                const cellX = startX + col * CONSTANTS.cellSize;
-                const cellY = startY + row * CONSTANTS.cellSize;
-                
-                const cell = this.nodeGrid.create(cellX, cellY, key);
-
-                this.scaleCell(cell);         
-            }
-        }
     },
 
     populateGrid: function(gridData){
@@ -58,18 +71,84 @@ const mainState = {
             const cellX = startX + col * CONSTANTS.cellSize;
             const cellY = startY + row * CONSTANTS.cellSize;
             
-            const cell = this.nodeGrid.create(cellX, cellY, gridMap[gridData[i]]);
-
-            this.scaleCell(cell);         
+            this.spawnCell(cellX, cellY, gridData[i]);
         }
     },
 
+    spawnCell:function(cellX, cellY, key){
+        if(this.nodeGrid !== null){
+            const cell = this.nodeGrid.create(cellX, cellY, gridToKey[key]);
+            this.scaleCell(cell);
+            cell._nodeType = key;
+            return cell;
+        }
+    },
     scaleCell: function(cell){
         const scaleFactorX = CONSTANTS.cellSize / cell.width
         const scaleFactorY = CONSTANTS.cellSize / cell.height
 
         cell.scale.set(scaleFactorX, scaleFactorY);
+    },
+
+    floodFillFrom: function(row, col){
+        let unFilled = [CONSTANTS.getIndex(row, col)];
+        let visited = new Set();
+
+        while(unFilled.length > 0){
+            const currentIndex = unFilled.shift();
+            visited.add(currentIndex);
+            const nextPoints = this.getConnections(currentIndex);
+            for(let i = 0; i < nextPoints.length; i++){
+                const nextIndex = CONSTANTS.getIndex(nextPoints[i].y, nextPoints[i].x);
+                if(!visited.has(nextIndex)){
+                    unFilled.push(nextIndex);
+                }
+            }
+            this.nodeGrid.getAt(currentIndex).tint = 0xff0000;
+        }
+    },
+    getConnections: function(index){
+        const potentialConnections = this.getPotentialConnections(index);
+        const rowCol = CONSTANTS.getRowCol(index);
+        const positionPoint = new Phaser.Point(rowCol.col, rowCol.row);
+        let connections = [];
+
+        for(let i = 0; i < potentialConnections.length; i++){
+            const neighbourPoint = Phaser.Point.add(positionPoint, potentialConnections[i]).round();
+            if(neighbourPoint.x >= 0 && neighbourPoint.x < CONSTANTS.gridWidth && neighbourPoint.y >= 0 && neighbourPoint.y < CONSTANTS.gridHeight){
+                const neighbourCell = this.nodeGrid.getAt(CONSTANTS.getIndex(neighbourPoint.y, neighbourPoint.x));
+                if(this.doesCellHaveConnection(neighbourCell, Phaser.Point.negative(potentialConnections[i]).round())){
+                    connections.push(neighbourPoint);
+                }
+            }
+        }
+        return connections;
+    },
+    getPotentialConnections: function(index){
+        const cell = this.nodeGrid.getAt(index);
+        const cellConnections = CONNECTIONS[cell._nodeType];
+        const cellRotation = cell.rotation;
+        let potentialConnections = [];
+
+        for(let i = 0; i < cellConnections.length; i++){
+            const potentialConnection = Phaser.Point.rotate(cellConnections[i], 0, 0, cellRotation).round();
+            potentialConnections.push(potentialConnection);
+        }
+        return potentialConnections;
+    },
+    doesCellHaveConnection: function(cell, direction){
+        let hasConnection = false;
+        const potentialConnections = this.getPotentialConnections(this.nodeGrid.getIndex(cell));
+
+        for(let i = 0; i < potentialConnections.length; i++){
+            if(Phaser.Point.equals(potentialConnections[i], direction)){
+                hasConnection = true;
+                break;
+            }
+        }
+        return hasConnection;
     }
+
 }
 // class Main extends Phaser.Scene{
 //     constructor(){
